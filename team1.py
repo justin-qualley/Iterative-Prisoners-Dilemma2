aacb1cf18b53485ceef0d3032432818a66410756
justin-qualley <19jquall30@isd77.k12.mn.us>
2017-11-30 14:56:04 -0600

    strat
    its kind of working

--- team1.py
+++ team1.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'IDK' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'the most op strat known to man'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
